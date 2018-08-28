783ff235f13f094912426d14b26fc47417f4cf1b
cjy <cjy632258@hotmail.com>
2018-08-28 11:05:22 +0800

    types

--- lib/client.d.ts
+++ lib/client.d.ts
@@ -1,9 +1,30 @@
 import Socket from 'socket.io-client'
 
 declare interface Client {
-  socket: Socket
+  socket: Socket,
+  subscribe: (channels: string | string[]) => Manager,
+  unsubscribe: (channels: string | string[]) => Manager,
+  connected: (cb?: Function) => Promise | void,
+  onError: (cb?: Function) => Promise | void,
+  broadcast: (channel: string | string[], ev : string | any, payload ?: any) => void,
+  on: (ev: string, cb : Function) => void,
+  emit: (ev: string,  payload : any) => void,
+  select: (channel: string | string[], create : boolean = true) => Channel | Manager,
+  main: () => Channel,
+}
+
+declare interface Channel {
+  name: string,
+  client: Client,
+  broadcast:  (ev : string | any, payload ?: any) => Channel,
+  on: (ev: string, cb : Function) => Channel,
 }
 
 declare interface Manager {
-
+  channels: Channel[],
+  broadcast:  (ev : string | any, payload ?: any) => Manager,
+  on: (ev: string, cb : Function) => Manager,
+  push: (channel : Channel) => Manager,
+  select: (channel: string | string[]) => Manager,
+  has: (channel: string ) => boolean,
 }
